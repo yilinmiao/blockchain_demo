a134455f6fb7b7c96b8583249b54eac30cb9b87a
yilinmiao <yi.matheson@gmail.com>
2018-11-05 23:06:30 -0800

    Create apis with Flask

--- blockchain.py
+++ blockchain.py
@@ -22,7 +22,7 @@ class Blockchain:
         block = {'index': len(self.chain) + 1,
                  'timestamp': str(datetime.datetime.now()),
                  'proof': proof,
-                 'previoius_hash': previous_hash}
+                 'previous_hash': previous_hash}
         self.chain.append(block)
         return block
     
@@ -61,4 +61,44 @@ class Blockchain:
         return True
 
 # Part 2 - Mining our Blockchains
-            
+
+# Creating a Web App
+app = Flask(__name__)
+    
+# Creating a Blockchain
+blockchain = Blockchain()
+
+# Mining a new block
+@app.route('/mine_block', methods = ['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'Congratulations, you just mined a block!',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
+
+# Getting the full Blockchain
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
+
+# Checking if the BLockchain is valid
+@app.route('/is_valid', methods = ['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message': 'All good. The Blockchain is valid'}
+    else:
+        response = {'message': 'The blockchain is invalid.'}
+    return jsonify(response), 200
+
+# Running the app
+app.run(host = '0.0.0.0', port = 5000)
